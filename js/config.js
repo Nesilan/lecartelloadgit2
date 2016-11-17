5ae930d5b6bf526daa25a695382ac1c3fd4e6b61
Nesilan <nicolas.gabaret713@gmail.com>
2016-11-17 13:26:18 +0100

    lecartelloadgit2

---
+++ js/config.js
@@ -0,0 +1,126 @@
+/*    _           _    _                     _ 
+     | |         | |  | |                   | |
+  ___| | ___  ___| | _| |     ___   __ _  __| |
+ / __| |/ _ \/ _ \ |/ / |    / _ \ / _` |/ _` |
+ \__ \ |  __/  __/   <| |___| (_) | (_| | (_| |
+ |___/_|\___|\___|_|\_\______\___/ \__,_|\__,_|
+    version 2.04
+*/
+
+/*URL for the server logo
+Leave it blank to use text instead*/
+var l_serverImage = "images/logo.png";
+
+/*Custom server name configuration
+Leave it blank for automatic server name*/
+var l_serverName = "ExtopiaRP";
+
+/*Use a video for the background?
+Otherwise it will just use images*/
+var l_bgVideo = false;
+
+/*Video background
+Avaiable options:
+"backgrounds/videos/aurora.webm"
+"backgrounds/videos/bluefall.webm"
+"backgrounds/videos/bluetilefloor.webm"
+"backgrounds/videos/bluewhitecircles.webm"
+"backgrounds/videos/bokehlens.webm"
+"backgrounds/videos/cleanbokeh.webm"
+"backgrounds/videos/coldnight.webm"
+"backgrounds/videos/colorfulburst.webm"
+"backgrounds/videos/dancingstars.webm"
+"backgrounds/videos/dropsflares.webm"
+"backgrounds/videos/focusorangebokeh.webm"
+"backgrounds/videos/glimmering.webm"
+"backgrounds/videos/glowingrings.webm"
+"backgrounds/videos/greencircles.webm"
+"backgrounds/videos/hues.webm"
+"backgrounds/videos/inthedeep.webm"
+"backgrounds/videos/modernflourish.webm"
+"backgrounds/videos/orangeaura.webm"
+"backgrounds/videos/warminside.webm"
+"backgrounds/videos/yellowgreen.webm"
+
+You can add custom backgrounds
+Just make sure it is at webm VP8 format */
+var l_background = "backgrounds/videos/glimmering.webm";
+
+/*(Image-background only)
+Place one or more images
+Multiple images will automatically shuffle between them*/
+var l_bgImages = [
+	"backgrounds/images/fond1.jpg",
+	"backgrounds/images/fond2.jpg",
+	"backgrounds/images/fond3.png",
+	"backgrounds/images/fond4.jpg",
+	"backgrounds/images/fond5.jpg"
+];
+
+/*Random background images order?*/
+var l_bgImagesRandom = false;
+
+/*(Image-background only)
+Delay between background images changes
+in milliseconds*/
+var l_bgImageDuration = 5000;
+
+/*(Image-background only)
+Image background fade velocity
+in milliseconds*/
+var l_bgImageFadeVelocity = 2000;
+
+/*Enable background overlay?*/
+var l_bgOverlay = true;
+
+/*Background darkening amount
+0 for none (0%), 100 for pitch black (100%)*/
+var l_bgDarkening = 50;
+
+/*Music player
+'true' or 'false' to enable music in the background*/
+var l_music = true;
+
+/*Display actual song's name?*/
+var l_musicDisplay = true;
+
+/*Music playlist
+Place how much Youtube IDs/.ogg you want*/
+var l_musicPlaylist = [
+	{ogg: "songs/[TV_On_The_Radio_-_DLZ_[Breaking_Bad_OST_[HQ_.ogg", name: "TV on the radio DLZ"},	
+	{ogg: "songs/Fever_Ray_-_If_I_Had_A_Heart.ogg", name: "Fever Ray If i had a heart"}	
+];
+
+/*Random music order?*/
+var l_musicRandom = false;
+
+/*Music volume
+Choose a value between 0 and 100*/
+var l_musicVolume = 40;
+
+/*Enable custom messages?*/
+var l_messagesEnabled = true;
+
+/*Enter your custom messages below*/
+var l_messages = [
+"Bienvenue sur LeCartel",
+"Merci de Ne pas Spam",
+"Merci de Ne pas Props Kill",
+"Merci de Ne pas Insulter",
+"Merci de Respecter le NRL",
+"Merci de Ne pas Use Bug",
+"Merci de Jouer RP",
+"Merci de ne pas Déco en Scène RP",
+"Enjoy !"
+];
+
+/*Random message order?*/
+var l_messagesRandom = false;
+
+/*Delay between message changes
+in milliseconds*/
+var l_messagesDelay = 5000;
+
+/*Messages fade time
+in milliseconds*/
+var l_messagesFade = 1000;
